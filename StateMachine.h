287521cb41bdbc075578617256b16f528a2088d9
Jason <jfern2011@gmail.com>
2017-11-03 15:16:16 -0700

    Forgot to push these into last commit

--- StateMachine.h
+++ StateMachine.h
@@ -1,60 +1,183 @@
 #ifndef __STATE_MACHINE__
 #define __STATE_MACHINE__
 
-class StateMachine
+#include "cmd.h"
+
+/**
+ * Represents a single task to perform while in a particular
+ * state
+ */
+template <class R, class... T>
+class Task : public Signal::Signal<R,T...>
 {
 
 public:
 
-	StateMachine()
+	/**
+	 * Constructor
+	 *
+	 * @param[in] name The name of this task
+	 */
+	Task(const std::string& name)
+		: _name(name)
 	{
 	}
 
-	virtual ~StateMachine()
+	/**
+	 * Destructor
+	 */
+	~Task()
 	{
+	}
+
+	/**
+	 * Get the name of this task
+	 *
+	 * @return The task name
+	 */
+	inline std::string get_name() const
+	{
+		return _name;
+	}
+
+	/**
+	 * Execute the task. This is generally called repeatedly
+	 * while in a particular state
+	 */
+	inline void run()
+	{
+		raise();
 	}
 
 private:
-	
+
+	/**
+	 * The name of this task
+	 */
+	std::string _name;
 };
 
-class xBoardStateMachine : public StateMachine
+/**
+ * A single state within a finite state machine
+ */
+class State
 {
+
+public:
+
+	State();
+	State(const std::string& name);
+
+	~State();
+
+	/**
+	 * Add to the list of tasks to be executed whenever we are
+	 * in this state
+	 *
+	 * @details
+	 * A task is essentially a routine that can be run in this
+	 * state. Thus, the template parameters determine the
+	 * signature of the function that will be invoked when the
+	 * task is performed
+	 *
+	 * @tparam R The task's return type
+	 * @tparam T Types of input arguments to the task
+	 *
+	 * @oaram [in] task A pointer to the task. Note this won't
+	 *                  transfer ownership of the pointer
+	 *
+	 * @return True on success
+	 */
+	template <typename R, typename... T>
+	bool add_task(Task<R,T...>* task)
+	{
+		AbortIf(task == nullptr, false);
+
+		_tasks.push_back(task);
+		return true;
+	}
+
+	std::string get_name() const;
+
+	bool run();
+
+private:
+
+	/**
+	 * The name of this state
+	 */
+	std::string _name;
+
+	/**
+	 * The set of tasks to perform while in this state
+	 */
+	std::vector<Signal::generic*>
+				_tasks;
+};
+
+/**
+ * Implements a finite state machine
+ */
+class StateMachine
+{
+	typedef std::vector<state_t> state_v;
 
 public:
 
 	typedef enum
 	{
-		READY,
-		FORCE
+		search,
+		ponder,
+		init,
+		idle,
+		num_states,
+		undef
 
 	} state_t;
 
-	xBoardStateMachine()
-		: _current_state(READY)
-	{
-	}
+	StateMachine();
 
-	~xBoardStateMachine()
-	{
-	}
+	~StateMachine();
 
-	state_t getState() const
-	{
-		return
-			_current_state;
-	}
+	bool build(int fd);
 
-	bool updateState(state_t state)
-	{
-		_current_state = state;
-		return true;
-	}
+	static bool get_transitions(state_t state,
+		state_v& transitions);
+
+	bool request_transition(
+		const std::string& state);
 
 private:
 
-	state_t
-		_current_state;
+	/**
+	 * The CommandInterface used to command state transitions
+	 */
+	CommandInterface
+		_cmd;
+
+	/**
+	 * Our current state machine state
+	 */
+	state_t _current_state;
+
+	/**
+	 * A mapping from state name to its enumeration
+	 */
+	std::map<std::string,state_t>
+		_name_to_id;
+
+	/**
+	 * All states in this state machine
+	 */
+	std::vector<State>
+		_states;
+	
+	/**
+	 * Mapping from state to a corresponding set of reachable
+	 * states
+	 */
+	std::vector<state_v>
+		_transitions;
 };
 
 #endif
--- engine.h
+++ engine.h
@@ -1,105 +1,22 @@
 #ifndef __ENGINE_H__
 #define __ENGINE_H__
 
-#include "cmd.h"
-#include "DataTables.h"
-#include "movegen.h"
-#include "position.h"
-#include "protocol.h"
-#include "search.h"
-#include "StateMachine.h"
 
-#include <iostream>
-#include <unistd.h>
-
-class ChessEngine : private xBoardStateMachine
+class ChessEngine
 {
 
 public:
 
-	ChessEngine(const DataTables& tables)
-		: _cmd(),
-		  _movegen(tables),
-		  _node(_movegen),
-		  _position(tables, true),
-		  _quit(false),
-		  _tables(tables),
-		  _xboard(_node, _position, *this)
-	{
-	}
+	ChessEngine();
 
-	~ChessEngine()
-	{
-	}
+	~ChessEngine();
 
-	bool init()
-	{
-		AbortIfNot(init_commands(), false);
-		AbortIfNot(_node.init(), false);
+	bool init();
 
-		std::cout << "Ready"  << std::endl;
-		return true;
-	}
-
-	bool run()
-	{
-		while (!_quit && !_node.quit_requested())
-		{
-			::usleep(100000); // 100 ms
-			_cmd.poll();
-		}
-
-		return true;
-	}
-
-	bool quit(const std::string& args)
-	{
-		_quit = true;
-		return _quit;
-	}
+	bool run();
 
 private:
 
-	bool init_commands()
-	{
-		AbortIf(_cmd.install("divide", _xboard, &xBoard::divide2) < 0,
-				false);
-		AbortIf(_cmd.install("go", _xboard, &xBoard::go) < 0,
-				false);
-		AbortIf(_cmd.install("usermove", _xboard, &xBoard::usermove) < 0,
-				false);
-		AbortIf(_cmd.install("new", _xboard, &xBoard::cmdNew) < 0,
-				false);
-		AbortIf(_cmd.install("nopost", _xboard, &xBoard::nopost) < 0,
-				false);
-		AbortIf(_cmd.install("perft", _xboard, &xBoard::perft2) < 0,
-				false);
-		AbortIf(_cmd.install("post", _xboard, &xBoard::post) < 0,
-				false);
-		AbortIf(_cmd.install("print", _xboard, &xBoard::print) < 0,
-				false);
-		AbortIf(_cmd.install("quit", *this, &ChessEngine::quit) < 0,
-				false);
-		AbortIf(_cmd.install("sd", _xboard, &xBoard::sd) < 0,
-				false);
-		AbortIf(_cmd.install("setboard", _xboard, &xBoard::setboard) < 0,
-				false);
-		AbortIf(_cmd.install("stat", _xboard, &xBoard::stat) < 0,
-				false);
-
-		return true;
-	}
-
-	CommandInterface _cmd;
-	MoveGen  _movegen;
-
-	// Note: _node requires a constructed
-	//       _movegen
-	Node     _node; 
-	Position _position;
-	bool     _quit;
-	const DataTables& _tables;
-	xBoard   _xboard;
 };
 
 #endif
