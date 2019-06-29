03b68bffbd998728bd150f71bfa55de63bcde3aa
jfern2011 <jfern2011@gmail.com>
2019-06-29 07:22:04 -0700

    Fixed indentation in OutputChannel header

--- src/OutputChannel.h
+++ src/OutputChannel.h
@@ -8,53 +8,53 @@
 
 namespace Chess
 {
-	/**
-	 * An abstract interface for sending engine outputs any
-	 * some user-defined destination
-	 */
-	class OutputChannel
-	{
+    /**
+     * An abstract interface for sending engine outputs any
+     * some user-defined destination
+     */
+    class OutputChannel
+    {
 
-	public:
+    public:
 
-		OutputChannel();
+        OutputChannel();
 
-		virtual ~OutputChannel();
+        virtual ~OutputChannel();
 
-		/**
-		 * Output stream operator. Note that mutliple
-		 * calls can be chained
-		 *
-		 * @tparam T The type of \a data
-		 *
-		 * @param [in] data The data element to write
-		 *
-		 * @return *this
-		 */
-		template <typename T>
-		OutputChannel& operator<<(const T& data)
-		{
-			std::string data_s;
-			AbortIfNot(Util::to_string(data, data_s), *this);
-			AbortIfNot(write(
-				data_s.c_str(), data_s.size()), *this);
-			return *this;
-		}
+        /**
+         * Output stream operator. Note that mutliple
+         * calls can be chained
+         *
+         * @tparam T The type of \a data
+         *
+         * @param [in] data The data element to write
+         *
+         * @return *this
+         */
+        template <typename T>
+        OutputChannel& operator<<(const T& data)
+        {
+            std::string data_s;
+            AbortIfNot(Util::to_string(data, data_s), *this);
+            AbortIfNot(write(
+                data_s.c_str(), data_s.size()), *this);
+            return *this;
+        }
 
-		/**
-		 * Write to the output channel
-		 *
-		 * @param[in] buf  The buffer to write
-		 * @param[in] size Number of bytes to write
-		 *
-		 * @return True on success
-		 */
-		virtual bool write(const char* buf,
-			size_t size) = 0;
+        /**
+         * Write to the output channel
+         *
+         * @param[in] buf  The buffer to write
+         * @param[in] size Number of bytes to write
+         *
+         * @return True on success
+         */
+        virtual bool write(const char* buf,
+            size_t size) = 0;
 
-	protected:
+    protected:
 
-	};
+    };
 }
 
 #endif
