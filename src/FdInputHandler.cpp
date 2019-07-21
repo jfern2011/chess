41420cfaf76b6f3ebef517b344e4608b93d436d8
jfern2011 <jfern2011@gmail.com>
2019-07-20 22:29:59 -0700

    Fixed logic in FdInputHandler

--- src/FdInputHandler.cpp
+++ src/FdInputHandler.cpp
@@ -30,34 +30,34 @@ namespace Chess
         pfd.fd     = _fd;
         pfd.events = POLLIN;
 
-        const int n_events = ::poll(&pfd, 1, timeout);
-        AbortIf(n_events < 0, false);
+        const int num_events = ::poll( &pfd, 1, timeout );
+        AbortIf(num_events < 0, false);
 
-        if (n_events)
+        if (num_events)
         {
             AbortIf(pfd.revents != POLLIN,
                 false);
-        }
 
-        int fsize;
-        AbortIf(::ioctl( _fd, FIONREAD, &fsize ) < 0,
-            false);
+            int fsize;
+            AbortIf(::ioctl( _fd, FIONREAD, &fsize ) < 0,
+                false);
 
-        if ( (size_t)fsize > _buf.size() )
-            _buf.resize(fsize);
+            if ( (size_t)fsize > _buf.size() )
+                _buf.resize(fsize);
 
-        AbortIf(::read(_fd, &_buf[0], fsize) < 0,
-            false);
+            AbortIf(::read(_fd, &_buf[0], fsize) < 0,
+                false);
 
-        std::vector< std::string > tokens; Util::split(
-            _buf, tokens, "\n");
+            std::vector< std::string > tokens; Util::split(
+                _buf, tokens, "\n");
 
-        if (input_signal)
-        {
-            for (auto& cmd : tokens)
+            if (input_signal)
             {
-                AbortIfNot(input_signal(cmd),
-                    false);
+                for (auto& cmd : tokens)
+                {
+                    AbortIfNot(input_signal(cmd),
+                        false);
+                }
             }
         }
 
