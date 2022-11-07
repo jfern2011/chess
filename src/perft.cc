3e84e10ed6e63fd078e238d2759151c32203a78b
jfern2011 <jfern2011@gmail.com>
2022-11-06 22:37:52 -0500

    Add perft arguments

--- src/perft.cc
+++ src/perft.cc
@@ -1,7 +1,30 @@
 #include <cstdlib>
+#include <iostream>
+#include <stdexcept>
 
 #include "argparse/argparse.hpp"
 
 int main(int argc, char** argv) {
+    argparse::ArgumentParser parser("perft");
+
+    parser.add_argument("--depth")
+        .help("The maximum recursive trace depth, in plies")
+        .required();
+    parser.add_argument("--divide")
+        .help("If true, generate divide() results")
+        .default_value(false)
+        .implicit_value(true);
+    parser.add_argument("--fen")
+        .help("The root position in Forsyth-Edwards notation")
+        .default_value("");
+    
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::runtime_error& error) {
+        std::cerr << error.what() << std::endl;
+        std::cerr << parser;
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
